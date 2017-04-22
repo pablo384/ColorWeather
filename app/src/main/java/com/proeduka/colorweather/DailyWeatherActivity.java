24871a9819a0f0273942fc57d5a21f7aa53252bd
pablo384 <pabloreinoso0@gmail.com>
2017-04-22 12:48:16 -0400

    Main Layout complete

---
+++ app/src/main/java/com/proeduka/colorweather/DailyWeatherActivity.java
@@ -0,0 +1,13 @@
+package com.proeduka.colorweather;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DailyWeatherActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daily_weather);
+    }
+}
