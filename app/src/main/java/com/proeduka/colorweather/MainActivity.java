03bfbfcfa543ccb4d192cc1f79f437f5342f7fe3
pablo384 <pabloreinoso0@gmail.com>
2017-04-22 13:31:30 -0400

    OnClic in Main Activity

--- app/src/main/java/com/proeduka/colorweather/MainActivity.java
+++ app/src/main/java/com/proeduka/colorweather/MainActivity.java
@@ -1,13 +1,40 @@
 package com.proeduka.colorweather;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
+    public static final String TAG=MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    private void declarateViews(){
+
+    }
+    public void dailyCLic(View v){
+        Intent intent = new Intent(MainActivity.this, DailyWeatherActivity.class);
+        startActivity(intent);
+        Log.d(TAG,"DailyTextView CLic");
+    }
+    public void HourlyCLic(View v){
+        Intent intent = new Intent(MainActivity.this, HourlyWeatherActivity.class);
+        startActivity(intent);
+        Log.d(TAG,"HourlyTextView CLic");
+    }
+    public void MinutelyCLic(View v){
+        Intent intent = new Intent(MainActivity.this, MinutlyWeatherActivity.class);
+        startActivity(intent);
+        Log.d(TAG,"MinutelyTextView CLic");
+    }
+
 }
