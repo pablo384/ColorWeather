ac0d169681dc373a291fb92b68ecfdea3d31d9bf
pablo384 <pabloreinoso0@gmail.com>
2017-04-22 13:02:40 -0400

    Minutly Layout complete

---
+++ app/src/main/java/com/proeduka/colorweather/MinutlyWeatherActivity.java
@@ -0,0 +1,13 @@
+package com.proeduka.colorweather;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MinutlyWeatherActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_minutly_weather);
+    }
+}
