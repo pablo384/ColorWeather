980a771f1d754cdc763dd2c64d97d9002db84e2f
pablo384 <pabloreinoso0@gmail.com>
2017-04-29 18:12:12 -0400

    DailyWeatherAdapter

---
+++ app/src/main/java/com/proeduka/colorweather/adapters/DailyWeatherAdapter.java
@@ -0,0 +1,31 @@
+package com.proeduka.colorweather.adapters;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by pablo384 on 29/04/17.
+ */
+
+public class DailyWeatherAdapter extends BaseAdapter {
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;//no se usara
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
