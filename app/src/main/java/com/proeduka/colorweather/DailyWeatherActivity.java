e976706ecb4565ebbc7836f72a516bb5028abbc4
pablo384 <pabloreinoso0@gmail.com>
2017-04-29 18:08:13 -0400

    ListActivity, clase Day

--- app/src/main/java/com/proeduka/colorweather/DailyWeatherActivity.java
+++ app/src/main/java/com/proeduka/colorweather/DailyWeatherActivity.java
@@ -1,13 +1,34 @@
 package com.proeduka.colorweather;
 
 import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
-public class DailyWeatherActivity extends Activity {
+import java.util.ArrayList;
+
+public class DailyWeatherActivity extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daily_weather);
+
+        ArrayList<String> daysArray = new ArrayList<String>();
+        daysArray.add("Lunes");
+        daysArray.add("Martes");
+        daysArray.add("Miercoles");
+        daysArray.add("Jueves");
+        daysArray.add("Viernes");
+        daysArray.add("Sabado");
+        daysArray.add("Domingo");
+
+
+        ArrayAdapter<String> daysAdapter =
+                new ArrayAdapter<String>(this,
+                        android.R.layout.simple_list_item_1,
+                        daysArray);
+
+        setListAdapter(daysAdapter);
     }
 }
---
+++ app/src/main/java/com/proeduka/colorweather/Day.java
@@ -0,0 +1,41 @@
+package com.proeduka.colorweather;
+
+/**
+ * Created by pablo384 on 29/04/17.
+ */
+
+public class Day {
+    private String dayName;
+    private String weatherDescription;
+    private String rainProbability;
+
+    public Day(String dayName, String weatherDescription, String rainProbability) {
+        this.dayName = dayName;
+        this.weatherDescription = weatherDescription;
+        this.rainProbability = rainProbability;
+    }
+
+    public String getDayName() {
+        return dayName;
+    }
+
+    public void setDayName(String dayName) {
+        this.dayName = dayName;
+    }
+
+    public String getWeatherDescription() {
+        return weatherDescription;
+    }
+
+    public void setWeatherDescription(String weatherDescription) {
+        this.weatherDescription = weatherDescription;
+    }
+
+    public String getRainProbability() {
+        return rainProbability;
+    }
+
+    public void setRainProbability(String rainProbability) {
+        this.rainProbability = rainProbability;
+    }
+}
