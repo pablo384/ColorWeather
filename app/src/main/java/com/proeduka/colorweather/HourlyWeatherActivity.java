9b08bdc0d4955e88e9ba505326a229bd2d4e8c97
pablo384 <pabloreinoso0@gmail.com>
2017-05-03 19:17:20 -0400

    Seting adapter in other form!

--- app/src/main/java/com/proeduka/colorweather/HourlyWeatherActivity.java
+++ app/src/main/java/com/proeduka/colorweather/HourlyWeatherActivity.java
@@ -3,17 +3,25 @@ package com.proeduka.colorweather;
 import android.app.Activity;
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 import com.proeduka.colorweather.adapters.HourlyWeatherAdapter;
 
 import java.util.ArrayList;
 
-public class HourlyWeatherActivity extends ListActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class HourlyWeatherActivity extends Activity {
+
+    @BindView(R.id.hourlyListView)
+    ListView hourlyListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hourly_weather);
+        ButterKnife.bind(this);
 
         ArrayList<Hour> hourArrayList = new ArrayList<Hour>();
 
@@ -25,7 +33,7 @@ public class HourlyWeatherActivity extends ListActivity {
 
         HourlyWeatherAdapter hourlyWeatherAdapter = new HourlyWeatherAdapter(hourArrayList,this);
 
-        setListAdapter(hourlyWeatherAdapter);
+        hourlyListView.setAdapter(hourlyWeatherAdapter);
 
 
 
