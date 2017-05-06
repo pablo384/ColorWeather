2abaac61bbc6da81b8eb4378a9ef04ecdbc06e4b
pablo384 <pabloreinoso0@gmail.com>
2017-05-06 12:53:32 -0400

    Fixing String URL

--- app/src/main/java/com/proeduka/colorweather/activities/MainActivity.java
+++ app/src/main/java/com/proeduka/colorweather/activities/MainActivity.java
@@ -78,7 +78,13 @@ public class MainActivity extends AppCompatActivity {
         // Instantiate the RequestQueue.
         RequestQueue queue = Volley.newRequestQueue(this);
         //String url = "https://api.darksky.net/forecast/1454f81bb70550586e66207bcc3aa8d3/19.223319, -70.513781?units=si";
-        String url = "https://api.darksky.net/forecast/1454f81bb70550586e66207bcc3aa8d3/37.8267,-122.4233?units=si";
+        String baseURL="https://api.darksky.net/forecast";
+        String apiKey = "1454f81bb70550586e66207bcc3aa8d3";
+        String latitude = "37.8267";
+        String longitude = "-122.4233";
+        String units = "units=si";
+        String url = baseURL+"/"+apiKey+"/"+latitude+","+longitude+"?"+units;
+        //String url = "https://api.darksky.net/forecast/1454f81bb70550586e66207bcc3aa8d3/37.8267,-122.4233?units=si";
 
         // Request a string response from the provided URL.
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
